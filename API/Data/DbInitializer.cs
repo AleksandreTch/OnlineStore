5ebeffb4d48740e0efb3c64daf94236f1d759af2
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-07-09 17:14:40 -0400

    Updated Private Fields

---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,207 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StoreContext context)
+        {
+            if(context.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+// --------------------------------C-O-R-D-S---------------------------
+                new Product
+                {
+                    Name = "iPhone Charging Cord",
+                    Description = 
+                        "Type C to lightning charging cord.",
+                    Price = 25,
+                    PictureUrl = "/images/products/cord-01.png",
+                    Brand = "Apple",
+                    Type = "Accessories",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "iPhone Charging Cord",
+                    Description = 
+                        "Type A to lightning charging cord.",
+                    Price = 25,
+                    PictureUrl = "/images/products/cord-02.png",
+                    Brand = "Apple",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Samsung Charging Cord",
+                    Description = 
+                        "Type C to type C charging cord.",
+                    Price = 30,
+                    PictureUrl = "/images/products/cord-03.png",
+                    Brand = "Belkin",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Samsung Charging Cord",
+                    Description = 
+                        "Type A to type C charging cord.",
+                    Price = 30,
+                    PictureUrl = "/images/products/cord-04.png",
+                    Brand = "Belkin",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+// ---------------------------------A-D-A-P-T-E-R-S---------------------
+                new Product
+                {
+                    Name = "iPhone dongle",
+                    Description = 
+                        "Aux to lightning adapter",
+                    Price = 20,
+                    PictureUrl = "/images/products/dongle-01.png",
+                    Brand = "Belkin",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Samsung dongle",
+                    Description = 
+                        "Aux to C adapter",
+                    Price = 30,
+                    PictureUrl = "/images/products/dongle-02.png",
+                    Brand = "Belkin",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+// ---------------------------------C-H-A-R-G-I-N-G--B-R-I-C-K-S--------
+                new Product
+                {
+                    Name = "Apple Charging Brick",
+                    Description = 
+                        "Type C to Wall",
+                    Price = 25,
+                    PictureUrl = "/images/products/brick-01.png",
+                    Brand = "Apple",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Apple Charging Brick & The Cord",
+                    Description = 
+                        "Type C to Wall with type C to lightning cord",
+                    Price = 50,
+                    PictureUrl = "/images/products/brick-02.png",
+                    Brand = "Apple",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Samsung Chargin Brick ",
+                    Description = 
+                        "Type C to Wall",
+                    Price = 20,
+                    PictureUrl = "/images/products/brick-03.png",
+                    Brand = "Samsung",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Samsung Chargin Brick & The Cord ",
+                    Description = 
+                        "Type C to Wall with type c to type c",
+                    Price = 50,
+                    PictureUrl = "/images/products/brick-03.png",
+                    Brand = "Samsung",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+// ---------------------------------H-E-A-D-P-H-O-N-E-S-----------------
+                new Product
+                {
+                    Name = "Aux Headphones",
+                    Description = 
+                        "Small compact earphones for everyday use",
+                    Price = 30,
+                    PictureUrl = "/images/products/headphones-01.png",
+                    Brand = "Skull Candy",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Aux Headphones",
+                    Description = 
+                        "Small compact earphones for everyday use",
+                    Price = 50,
+                    PictureUrl = "/images/products/headphones-02.png",
+                    Brand = "Skull Candy",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Aux Headphones",
+                    Description = 
+                        "Small compact earphones for everyday use",
+                    Price = 15,
+                    PictureUrl = "/images/products/headphones-03.png",
+                    Brand = "AKG",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Apple Aux Headphones",
+                    Description = 
+                        "Small compact earphones for everyday use",
+                    Price = 50,
+                    PictureUrl = "/images/products/headphones-05.png",
+                    Brand = "Apple",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Apple Lightning Headphones",
+                    Description = 
+                        "Small compact earphones for everyday use",
+                    Price = 30,
+                    PictureUrl = "/images/products/headphones-06.png",
+                    Brand = "Apple",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+                new Product
+                {
+                    Name = "Type C Headphones",
+                    Description = 
+                        "Small compact earphones for everyday use",
+                    Price = 30,
+                    PictureUrl = "/images/products/headphones-07.png",
+                    Brand = "Skullcandy",
+                    Type = "Accessories",
+                    QuantityInStock = 100  
+                },
+            };
+
+            foreach (var product in products)
+            {
+                context.Products.Add(product);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
