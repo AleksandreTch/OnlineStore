9a332ecf50dffbf61720cef8cc662e08e213310b
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-07-13 11:21:45 -0400

    Added REACT Roots! NOW VIEW PRODUCT IS WORKING!

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,13 @@
 import { Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import { useState } from "react";
+import { Route, BrowserRouter, Switch, Router} from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
+
 
 function App() {
   const[darkMode, setDarkMode] = useState(false);
@@ -19,12 +25,17 @@ function App() {
     setDarkMode(!darkMode);
   }
 
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline/>
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-        <Catalog/>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/catalog' component={Catalog}/>
+          <Route path='/catalog/:id' component={ProductDetails}/>
+          <Route path='/about' component={AboutPage}/>
+          <Route path='/contact' component={ContactPage}/>
       </Container>
     </ThemeProvider>
   );
