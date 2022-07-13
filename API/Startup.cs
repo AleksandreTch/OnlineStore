1dcacb10982bde85d7f347d02f0b8d1ceb651815
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-07-13 15:25:59 -0400

    Adding Basket Components

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using System;
+using API.Controllers.Middleware;
 using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -38,9 +40,10 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {   
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
